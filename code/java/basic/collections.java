6655c460f9a0f49dee0c94b385b4361559599747
ELI JOSEPH BRADLEY <EtherTyper@users.noreply.github.com>
2017-05-21 16:01:10 -0500

    Use JEP 269 Collection factory methods.

--- code/java/basic/collections.java
+++ code/java/basic/collections.java
@@ -1,6 +1,5 @@
-final List&ltInteger&gt numbers = Arrays.asList(1, 2, 3);
+final List&ltInteger&gt numbers = List.of(1, 2, 3);
 
-final Map&ltInteger, String&gt map = new HashMap&ltInteger, String&gt();
-map.put(1, "One");
-map.put(2, "Two");
-map.put(3, "Three");
+final Map&ltInteger, String&gt map = Map.of(1, "One",
+                                            2, "Two",
+                                            3, "Three");
