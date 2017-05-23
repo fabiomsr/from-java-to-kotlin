e312b8e753c2fe762b3d3f344250eb1da76cbfef
ELI JOSEPH BRADLEY <EtherTyper@users.noreply.github.com>
2017-05-22 19:30:55 -0500

    Format collections.java properly.
    The HTML entities for > and < threw me off.

--- code/java/basic/collections.java
+++ code/java/basic/collections.java
@@ -10,5 +10,5 @@ map.put(3, "Three");
 final List&ltInteger&gt numbers = List.of(1, 2, 3);
 
 final Map&ltInteger, String&gt map = Map.of(1, "One",
-                                            2, "Two",
-                                            3, "Three");
+                                        2, "Two",
+                                        3, "Three");
